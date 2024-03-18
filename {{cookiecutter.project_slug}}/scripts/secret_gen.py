4f8c01dd4cdab48f8d630713b17b3ae6b86f8a15
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-03-17 23:59:51 -0300

    fix create secret key

--- {{cookiecutter.project_slug}}/scripts/secret_gen.py
+++ {{cookiecutter.project_slug}}/scripts/secret_gen.py
@@ -2,6 +2,6 @@ import secrets
 
 
 def secret_gen(length=50):
-    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_+)"
     secret_key = ''.join(secrets.choice(alphabet) for _ in range(length))
     return secret_key
