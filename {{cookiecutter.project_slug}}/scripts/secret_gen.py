b938bd694413294e105bc3bb34de5b16ff72eecb
Matheus Farias da Silva <matheusfarias009@hotmail.com>
2024-03-17 23:51:59 -0300

    fix function name

--- {{cookiecutter.project_slug}}/scripts/secret_gen.py
+++ {{cookiecutter.project_slug}}/scripts/secret_gen.py
@@ -1,7 +1,7 @@
 import secrets
 
 
-def generate_secret_key(length=50):
+def secret_gen(length=50):
     alphabet = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
     secret_key = ''.join(secrets.choice(alphabet) for _ in range(length))
     return secret_key
